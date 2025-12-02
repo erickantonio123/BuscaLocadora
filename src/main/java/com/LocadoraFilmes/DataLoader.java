82826cddd158cdad3fe3f363f33fcddf5b1b25f3
erickantonio123 <erickapmercado@hotmail.com>
2025-12-01 22:33:50 -0300

    Commit inicial sem segredos
    B

---
+++ src/main/java/com/LocadoraFilmes/DataLoader.java
@@ -0,0 +1,95 @@
+package com.LocadoraFilmes;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DataLoader implements CommandLineRunner {
+
+    private final GeneroRepository generoRepository;
+    private final PlataformaRepository plataformaRepository;
+    private final RollsRepository rollsRepository;
+    private final UsuarioRepository usuarioRepository;
+    private final LocadoraRepository locadoraRepository;
+    private final org.springframework.security.crypto.password.PasswordEncoder passwordEncoder;
+
+    public DataLoader(GeneroRepository generoRepository,
+                      RollsRepository rollsRepository,
+                      UsuarioRepository usuarioRepository,
+                     LocadoraRepository locadoraRepository,
+                     PlataformaRepository plataformaRepository,
+                      org.springframework.security.crypto.password.PasswordEncoder passwordEncoder) {
+        this.generoRepository = generoRepository;
+        this.rollsRepository = rollsRepository;
+        this.usuarioRepository = usuarioRepository;
+        this.passwordEncoder = passwordEncoder;
+        this.locadoraRepository = locadoraRepository;
+        this.plataformaRepository = plataformaRepository;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        // gêneros (mantive sua lógica)
+        java.util.List<String> generos = java.util.List.of("Ação","Comédia","Drama","Terror","Romance","Suspense","Ficção");
+        for (String nomeGenero : generos) {
+            if (generoRepository.findByNome(nomeGenero).isEmpty()) {
+                generoRepository.save(new Genero(nomeGenero));
+            }
+        }
+
+   java.util.List<String> plataformas = java.util.List.of("Disney+", "Netflix", "Amazon Prime", "HBO Max", "Star plus", "Apple TV", "Paramount+", "Globoplay");
+for (String nomePlataforma : plataformas) {
+        if (plataformaRepository.findByNome(nomePlataforma).isEmpty()) {
+            Plataforma plataforma = new Plataforma(nomePlataforma);
+            plataformaRepository.save(plataforma);
+            System.out.println("✅ Plataforma criada: " + nomePlataforma);
+        }
+    }
+        /* if (plataformaRepository.findByNome(nomePlataforma).isEmpty()) {
+            Plataforma plataforma = new Plataforma(nomePlataforma);
+            plataformaRepository.save(plataforma);
+            System.out.println("✅ Plataforma criada: " + nomePlataforma);
+        }
+        */
+    
+
+System.out.println("=== PLATAFORMAS NO BANCO ===");
+plataformaRepository.findAll().forEach(plat -> {
+    System.out.println("ID: " + plat.getId() + " | Nome: " + plat.getNome());
+});
+
+
+// Salva somente se não tiver nenhum filme cadastrado
+
+        // Roles
+        Role roleUser = rollsRepository.findByNome("ROLE_USER")
+                .orElseGet(() -> rollsRepository.save(new Role("ROLE_USER")));
+        Role roleAdmin = rollsRepository.findByNome("ROLE_ADMIN")
+                .orElseGet(() -> rollsRepository.save(new Role("ROLE_ADMIN")));
+
+        // Admin user
+        if (usuarioRepository.findByUsername("admin").isEmpty()) {
+    Usuario admin = new Usuario("admin", passwordEncoder.encode("123456"));
+    admin.getRoles().add(roleAdmin);
+    admin.getRoles().add(roleUser);
+    usuarioRepository.save(admin);
+    System.out.println("Admin criado com roles: " + admin.getRoles()); 
+}
+
+if (usuarioRepository.findByUsername("cliente").isEmpty()) {
+        Usuario user = new Usuario("cliente", passwordEncoder.encode("123456"));
+        user.getRoles().add(roleUser);
+        usuarioRepository.save(user);
+        System.out.println("✅ Usuário criado com role USER");
+    }
+    }
+
+
+    
+
+
+    
+}
---
+++ src/main/java/com/LocadoraFilmes/Locadora.java
@@ -0,0 +1,86 @@
+    package com.LocadoraFilmes;
+
+    import jakarta.persistence.Entity;
+    import jakarta.persistence.GeneratedValue;
+    import jakarta.persistence.GenerationType;
+    import jakarta.persistence.Id;
+    import jakarta.persistence.JoinColumn;
+    import jakarta.persistence.ManyToOne;
+    import jakarta.validation.constraints.NotBlank;
+    import jakarta.validation.constraints.Size;
+
+    @Entity
+    public class Locadora {
+
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        private Long id;
+
+            
+        @NotBlank(message = "O nome do filme é obrigatório")
+        @Size(min=2, max =100, message = "o nome  deve ter entre 2 ou 20 letras")
+        private  String nome;
+    
+
+    @ManyToOne
+        @JoinColumn(name = "genero_id")
+        private Genero genero;
+
+@ManyToOne
+@JoinColumn(name = "plataforma_id")
+private Plataforma plataforma;
+
+
+        public Locadora() {
+        }
+
+        public Locadora(String nome  ) {
+            this.nome = nome;
+        
+        
+        }
+public Locadora(String nome, Genero genero, Plataforma plataforma) {
+    this.nome = nome;
+    this.genero = genero;
+    this.plataforma = plataforma;
+}
+
+public Plataforma getPlataforma() {
+    return plataforma;
+}
+
+public void setPlataforma(Plataforma plataforma) {
+    this.plataforma = plataforma;
+}
+
+    
+        public String getNome() {
+            return nome;
+        }
+
+    
+        public void setNome(String nome) {
+            this.nome = nome;
+        }
+
+        public Long getId() {
+            return id;
+        }
+
+        public void setId(Long id) {
+            this.id = id;
+        }
+
+        public Genero getGenero() {
+            return genero;
+        }
+
+        public void setGenero(Genero genero) {
+            this.genero = genero;
+        }
+
+        
+    
+
+
+    }
